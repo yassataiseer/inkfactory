ef2b3a019223b16e07781b4237b5e3ca6d63653c
yassataiseer <1taiseeryas@hdsb.ca>
2020-09-11 17:47:18 -0400

    commit on 'master'

---
+++ recreate.py
@@ -0,0 +1,23 @@
+
+  
+import sqlite3
+import time
+import datetime
+import random
+
+conn = sqlite3.connect('employees.db', check_same_thread=False)
+c = conn.cursor()
+
+
+
+
+class data_answer:
+    def data(first_name, laste_name,password,date):
+        c.execute("UPDATE stuffToPlot SET ('Mohammed','Taiseer','56Operahouse','05-Oct-2015','05-Oct-2015')")
+
+        return( send_data)
+
+            
+
+
+
