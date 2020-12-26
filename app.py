951861b6455fd1da0b56a9e0a49bef724a4edc03
yassataiseer <1taiseeryas@hdsb.ca>
2020-12-26 16:12:23 -0500

    commit on add

--- app.py
+++ app.py
@@ -241,7 +241,8 @@ def change_order():
             order_data = order_writer.order_finder(order_No)
             hello = "wassup"
             html = render_template("customer_reciept.html",order_data=order_data,hello = hello)
-            config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+            #config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+            config = pdfkit.configuration(wkhtmltopdf="./bin/wkhtmltopdf.exe")
             #pdf = pdfkit.from_string(html, 'output.pdf',configuration=config)
             pdf = pdfkit.from_string(html, False, configuration=config)
             response = make_response(pdf)
